7b3901782a6b879e5ef4e21ca3a0aec45c75528d
Teodor Simchev <tsimchev@vmware.com>
2019-06-03 14:53:34 +0300

    Update vCenter port exteranl ID to point to the OpenStack port id

--- networking_nsxv3/plugins/ml2/drivers/nsxv3/agent/nsxv3_migration.py
+++ networking_nsxv3/plugins/ml2/drivers/nsxv3/agent/nsxv3_migration.py
@@ -67,6 +67,8 @@ class NSXv3NVDsMigrator(object):
                                             port.get("device_id"))
         vim_nic = vsphere.get_vm_nic_by_mac(vm_obj=vim_vm,
                                             macAddress=port.get("mac_address"))
+        # Change NIC external ID to the OpenStack Port
+        vim_nic.key = port.get("id")
         nsx_ls_spec = LogicalSwitch(id=nsx_ls_id)
         nsx_ls = nsxv3.get(sdk_service=LogicalSwitches, sdk_model=nsx_ls_spec)
 
