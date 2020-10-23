cfd3f94f7284781b6d2ce9e2da47ee55314194e0
Andrew Karpow <andrew.karpow@sap.com>
2020-10-23 16:24:46 +0200

    fix runner id enqueing

--- networking_nsxv3/plugins/ml2/drivers/nsxv3/agent/nsxv3_agent.py
+++ networking_nsxv3/plugins/ml2/drivers/nsxv3/agent/nsxv3_agent.py
@@ -336,7 +336,7 @@ class NSXv3AgentManagerRpcCallBackBase(amb.CommonAgentManagerRpcCallBackBase):
         except Exception as e:
             LOG.error(e)
             eventlet.sleep(10)
-            self.runner.run(sync.Priority.HIGHEST, port_id, self.sync_port)
+            self.runner.run(sync.Priority.HIGHEST, [port_id], self.sync_port)
 
 
     def sync_port_orphaned(self, port_id):
@@ -437,7 +437,7 @@ class NSXv3AgentManagerRpcCallBackBase(amb.CommonAgentManagerRpcCallBackBase):
                     physical_network=None, segmentation_id=None):
         LOG.debug("Updating Port='%s' with Segment='%s'", str(port),
                   segmentation_id)
-        self.runner.run(sync.Priority.HIGHEST, port["id"], self.sync_port)
+        self.runner.run(sync.Priority.HIGHEST, [port["id"]], self.sync_port)
 
     def _port_update(self, context, port=None, network_type=None,
                     physical_network=None, segmentation_id=None):
