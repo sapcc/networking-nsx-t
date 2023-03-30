fba4398c8d05b3be0e6c07b9d65d5db172733754
Sven Rosenzweig <sven.rosenzweig@sap.com>
2023-03-30 11:48:21 +0200

    slow query performance tuning for queries fetching revision information
    Checking the slow performance log of our neutron database revealed that 20-30% of the slow queries go back to nsxt queries fetching information about objects ports, security_groups and qos enriching them
    with revison information. Every nsxt agent runs those queries reguarly to determine orphand or outdated ports, security groups, qos_policies.
    Performance could be improved by applying a filter for the standardattribute table containg the revision information. Since the table contains siginficantlly more entries than the other tables (million vs. thousand entries), the number entires joined with the object information dropped.
    For all queries pagination has been implemented. Since the security_groups query did not handle duplicate values (due to the m-to-n relathionship between sg and ports), agents with a high number of sgs and ports, have been found triggering the query executiion multiple times
    until all data are fetched.

--- networking_nsxv3/db/db.py
+++ networking_nsxv3/db/db.py
@@ -27,6 +27,7 @@ def get_ports_with_revisions(context, host, limit, cursor):
         Port.standard_attr_id.asc()
     ).filter(
         StandardAttribute.id == Port.standard_attr_id,
+        StandardAttribute.resource_type == 'ports',
         PortBindingLevel.host == host,
         PortBindingLevel.driver == nsxv3_constants.NSXV3,
         Port.standard_attr_id > cursor,
@@ -50,6 +51,7 @@ def get_qos_policies_with_revisions(context, host, limit, cursor):
         QosPolicy.standard_attr_id.asc()
     ).filter(
         StandardAttribute.id == QosPolicy.standard_attr_id,
+        StandardAttribute.resource_type == 'qos_policies',
         PortBindingLevel.host == host,
         PortBindingLevel.driver == nsxv3_constants.NSXV3,
         QosPolicy.standard_attr_id > cursor
@@ -72,11 +74,12 @@ def get_security_groups_with_revisions(context, host, limit, cursor):
         sg_db.SecurityGroup.standard_attr_id.asc()
     ).filter(
         StandardAttribute.id == sg_db.SecurityGroup.standard_attr_id,
+        StandardAttribute.resource_type == 'securitygroups',
         PortBindingLevel.host == host,
         PortBindingLevel.level == 1,
         PortBindingLevel.driver == nsxv3_constants.NSXV3,
         sg_db.SecurityGroup.standard_attr_id > cursor
-    ).limit(
+    ).distinct().limit(
         limit
     ).all()
 
